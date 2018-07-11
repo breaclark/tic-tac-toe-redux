a4fdfdfe48869be93971d9f5bee8de4112c4cb23
Brea Clark and Matt Dowdney <brea@>
2018-07-11 13:51:00 -0700

    add tests for both reducers

--- _tests_/reducers/add-move-reducer.test.js
+++ _tests_/reducers/add-move-reducer.test.js
@@ -1,7 +1,31 @@
 import addMoveReducer from './../../src/reducers/add-move-reducer';
+import c from './../../src/constants';
 
 describe('addMoveReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(addMoveReducer({}, { type: null})).toEqual({});
   });
+
+  let action;
+  test('Should successfully change square value to X', () => {
+    action = {
+      type: c.ADD_MOVE,
+      squares: [null, null, null, null, null, null, null, null, 'X']
+    };
+    expect(addMoveReducer(Array(9).fill(null), action)).toEqual(
+      [null, null, null, null, null, null, null, null, 'X']
+    );
+  });
+
+  test('Should successfully change square value to O', () => {
+    action = {
+      type: c.ADD_MOVE,
+      squares: [null, null, null, 'O', null, null, null, null, null]
+    };
+    expect(addMoveReducer(Array(9).fill(null), action)).toEqual(
+      [null, null, null, 'O', null, null, null, null, null]
+    );
+  });
+
+
 });
--- _tests_/reducers/switch-player-reducer.test.js
+++ _tests_/reducers/switch-player-reducer.test.js
@@ -1,7 +1,19 @@
 import switchPlayerReducer from './../../src/reducers/switch-player-reducer';
+import c from './../../src/constants';
 
 describe('switchPlayerReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(switchPlayerReducer({}, { type: null})).toEqual({});
+  });
+
+  let action;
+  test('Should successfully change player turn value to false', () => {
+    action = {
+      type: c.SWITCH_PLAYER,
+      xIsNext: false
+    };
+    expect(switchPlayerReducer(true, action)).toEqual(
+      false
+    );
   });
 });
